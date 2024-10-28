201d220c9fbcd7b4b22818bf9e9051ade18e7932
mecha1977at <mecha1977at@gmail.com>
2024-10-28 11:31:54 +0100

    test

--- .netlify/functions-internal/appointments.js
+++ .netlify/functions-internal/appointments.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
@@ -9,18 +9,22 @@ exports.handler = async (event) => {
     };
   }
 
-  const newAppointment = JSON.parse(event.body);
-
-  // Stelle sicher, dass der Pfad zur appointments.json korrekt ist
-  const filePath = path.resolve(__dirname, '../../src/_data/appointments.json');
-
   try {
+    // Parse die Anfrage, um die gesendeten Daten zu erhalten
+    const newAppointment = JSON.parse(event.body);
+
+    // Pfad zur appointments.json Datei
+    const filePath = path.resolve(
+      __dirname,
+      '../../src/_data/appointments.json',
+    );
+
     // Lese die bestehende appointments.json Datei
     const fileContent = fs.readFileSync(filePath, 'utf8');
     let appointmentsData = JSON.parse(fileContent);
 
-    // Füge den neuen Termin zur Liste hinzu
-    appointmentsData.appointment.push(newAppointment);
+    // Füge den neuen Termin hinzu
+    appointmentsData[0].appointment.push(newAppointment);
 
     // Schreibe die aktualisierten Daten zurück in appointments.json
     fs.writeFileSync(filePath, JSON.stringify(appointmentsData, null, 2));
